c7a4c5fd511527478d8566b11f2d84b96b7ac49a
Prashant Salgaocar <prashant.salgaocar@gmail.com>
2019-08-08 12:34:30 -0700

    Made a change to App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome Prashant Salgaocar, to get started <code>src/App.js</code> and save to reload.
+          Welcome Prashant Prakash Salgaocar, to get started <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
